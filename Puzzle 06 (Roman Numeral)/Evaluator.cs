d7018f440caecc91ba8c40dfdc0450d4224f0835
TerryH <terry@tnthughes.net>
2010-12-01 21:36:47 -0800

    Updated the evaluator object in Puzzle 06 (Roman Numeral) to evaluate individual roman numerals

--- Puzzle 06 (Roman Numeral)/Evaluator.cs
+++ Puzzle 06 (Roman Numeral)/Evaluator.cs
@@ -6,10 +6,22 @@
 
 namespace THughes.Puzzles.RomanNumeral
 {
+    using System;
+    using System.Linq;
+
     /// <summary>
     /// Evaluates things.
     /// </summary>
     public static class Evaluator
     {
+        /// <summary>
+        /// Evaluates the roman numeral text into it's <see cref="Int32" /> value.
+        /// </summary>
+        /// <param name="value">The roman numeral text.</param>
+        /// <returns>The roman numeral text as a <see cref="Int32" />.</returns>
+        public static int Evaluate(string value)
+        {
+            return RomanNumeralToken.Tokens.First(t => value == t.Lexeme).NumericValue;
+        }
     }
 }
--- Puzzle 06 (Roman Numeral)/EvaluatorSpecifications.cs
+++ Puzzle 06 (Roman Numeral)/EvaluatorSpecifications.cs
@@ -7,9 +7,26 @@
 namespace THughes.Puzzles.RomanNumeral
 {
     using System.Diagnostics.CodeAnalysis;
+    using Machine.Specifications;
 
     [SuppressMessage("Microsoft.StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "Reviewed. Suppression is OK here.")]
+    [SuppressMessage("Microsoft.StyleCop.CSharp.MaintainabilityRules", "SA1400:AccessModifierMustBeDeclared", Justification = "Reviewed. Suppression is OK here.")]
+    [SuppressMessage("Microsoft.StyleCop.CSharp.NamingRules", "SA1310:FieldNamesMustNotContainUnderscore", Justification = "Reviewed. Suppression is OK here.")]
+    [SuppressMessage("Microsoft.StyleCop.CSharp.SpacingRules", "SA1003:SymbolsMustBeSpacedCorrectly", Justification = "Reviewed. Suppression is OK here.")]
+    [SuppressMessage("Microsoft.StyleCop.CSharp.SpacingRules", "SA1009:ClosingParenthesisMustBeSpacedCorrectly", Justification = "Reviewed. Suppression is OK here.")]
     public class EvaluatorSpecifications
     {
+#pragma warning disable 169
+// ReSharper disable InconsistentNaming
+        It can_evaluate_each_roman_numeral_individually =()=>
+        {
+            foreach (var tuple in Build.IndividualValues())
+            {
+                Evaluator.Evaluate(tuple.Item1).ShouldEqual(tuple.Item2);
+            }
+        };
+
+// ReSharper restore InconsistentNaming
+#pragma warning restore 169
     }
 }
