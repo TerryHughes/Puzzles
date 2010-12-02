1505aecf9e500321c443bb1c571a080c1163fb6e
TerryH <terry@tnthughes.net>
2010-12-01 22:34:31 -0800

    Added a build object to Puzzle 06 (Roman Numeral)

---
+++ Puzzle 06 (Roman Numeral)/Build.cs
@@ -0,0 +1,15 @@
+//-----------------------------------------------------------------------
+// <copyright file="Build.cs" company="THughes">
+//     Copyright (c) THughes. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace THughes.Puzzles.RomanNumeral
+{
+    using System.Diagnostics.CodeAnalysis;
+
+    [SuppressMessage("Microsoft.StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "Reviewed. Suppression is OK here.")]
+    internal static class Build
+    {
+    }
+}
