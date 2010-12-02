1bff7934c9ddf06bd356dcd8c512f81b1077277b
TerryH <terry@tnthughes.net>
2010-12-01 22:35:24 -0800

    Added an evaluator object to Puzzle 06 (Roman Numeral)

---
+++ Puzzle 06 (Roman Numeral)/Evaluator.cs
@@ -0,0 +1,15 @@
+//-----------------------------------------------------------------------
+// <copyright file="Evaluator.cs" company="THughes">
+//     Copyright (c) THughes. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace THughes.Puzzles.RomanNumeral
+{
+    /// <summary>
+    /// Evaluates things.
+    /// </summary>
+    public static class Evaluator
+    {
+    }
+}
