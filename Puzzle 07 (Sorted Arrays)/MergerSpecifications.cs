134eb0be609089d56b05d7db8b05a7bc02b13048
TerryH <terry@tnthughes.net>
2010-12-17 07:29:35 -0800

    Updated specification for merge object in Puzzle 07 (Sorted Arrays) to verify that multiple arrays merge correctly

--- Puzzle 07 (Sorted Arrays)/MergerSpecifications.cs
+++ Puzzle 07 (Sorted Arrays)/MergerSpecifications.cs
@@ -6,6 +6,7 @@
 
 namespace THughes.Puzzles.SortedArrays
 {
+    using System.Collections.Generic;
     using System.Diagnostics.CodeAnalysis;
     using System.Linq;
     using Machine.Specifications;
@@ -26,6 +27,34 @@ namespace THughes.Puzzles.SortedArrays
 
 #pragma warning disable 169
         Because of =()=> result = Merger.Merge(new[] { odd, even });
+
+        It sorts_the_elements_in_the_array =()=> result.SequenceEqual(array).ShouldBeTrue();
+#pragma warning restore 169
+    }
+
+    [SuppressMessage("Microsoft.StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "Reviewed. Suppression is OK here.")]
+    [SuppressMessage("Microsoft.StyleCop.CSharp.MaintainabilityRules", "SA1400:AccessModifierMustBeDeclared", Justification = "Reviewed. Suppression is OK here.")]
+    [SuppressMessage("Microsoft.StyleCop.CSharp.MaintainabilityRules", "SA1402:FileMayOnlyContainASingleClass", Justification = "Reviewed. Suppression is OK here.")]
+    [SuppressMessage("Microsoft.StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Reviewed. Suppression is OK here.")]
+    [SuppressMessage("Microsoft.StyleCop.CSharp.NamingRules", "SA1310:FieldNamesMustNotContainUnderscore", Justification = "Reviewed. Suppression is OK here.")]
+    [SuppressMessage("Microsoft.StyleCop.CSharp.SpacingRules", "SA1003:SymbolsMustBeSpacedCorrectly", Justification = "Reviewed. Suppression is OK here.")]
+    [SuppressMessage("Microsoft.StyleCop.CSharp.SpacingRules", "SA1009:ClosingParenthesisMustBeSpacedCorrectly", Justification = "Reviewed. Suppression is OK here.")]
+    public class when_one_through_nine_is_divided_into_nine_arrays
+    {
+        static readonly int[] array = Enumerable.Range(1, 9).ToArray();
+        static int[] result;
+
+#pragma warning disable 169
+        Because of =()=>
+        {
+            var arrays = new List<int[]>();
+            for (var i = 0; i < array.Length; i++)
+            {
+                arrays.Add(array.Skip(i).Take(1).ToArray());
+            }
+
+            result = Merger.Merge(arrays);
+        };
 
         It sorts_the_elements_in_the_array =()=> result.SequenceEqual(array).ShouldBeTrue();
 #pragma warning restore 169
